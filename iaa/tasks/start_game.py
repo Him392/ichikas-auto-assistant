a31d714a8ede77fd9628cf8dfd39c96166f1a726
XcantloadX <3188996979@qq.com>
2025-08-04 19:03:45 +0800

    feat(task): 启动游戏支持处理数据更新弹窗

--- iaa/tasks/start_game.py
+++ iaa/tasks/start_game.py
@@ -43,6 +43,10 @@ def go_home(threshold_timeout: float = 0):
                 break
         else:
             cd.reset()
+        # 有新需要数据下载
+        if image.find(R.Login.TextRecommendDownloadViaWifi):
+            device.click(image.expect(R.Login.ButtonDownload))
+        
         if th.request():
             device.click(1, 1)
 
